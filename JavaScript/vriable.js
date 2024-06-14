34b98e45478b7b3643996074fc84da8ec440ac2a
Muhammad-Shariq-Khan <shariqkhan.97541@gmail.com>
2024-06-14 14:46:32 +0000

    JavaScript Vriable

---
+++ JavaScript/vriable.js
@@ -0,0 +1,49 @@
+const Email = "Shariq@gamil.com"; //No change
+let UserName = "Shariq";
+var password = 1478;
+city = "Dewas";
+
+
+
+// Email = "Sami@gmail.com";
+UserName = "Sami";
+password = 1212;
+city = "Indore";
+
+
+console.table([Email, UserName, password, city])
+console.log(Email)
+
+let Name; //Declaring
+Name = "Jone"; //Assignment
+let Name = "Jone Dev" //Initialization
+
+console.log(Name);
+
+
+function f1() {
+    var x = 10;
+
+    if (x == 10) {
+        let y = 12
+        console.log("x=" + x + " " + "y=" + y)
+    }
+    else {
+        console.log("fail")
+    }
+}
+
+console.log(f1())
+
+const i;
+i = "Jone";
+
+console.log(i)
+
+x = 10;
+console.log(x)
+var x;
+
+x = 15;
+console.log(x);
+let x;
